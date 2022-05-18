ab2de18173951594c43c1ad164ce481379ee86d5
achrt <aache154@mts.ru>
2022-05-18 23:09:38 +0300

    изменение обработки флага -r

--- cmd/server/application/config.go
+++ cmd/server/application/config.go
@@ -19,14 +19,16 @@ type Config struct {
 	Address       string        `env:"ADDRESS" envDefault:"127.0.0.1:8080"`
 	StoreFile     string        `env:"STORE_FILE" envDefault:"/tmp/devops-metrics-db.json"`
 	StoreInterval time.Duration `env:"STORE_INTERVAL" envDefault:"300s"`
-	Restore       bool          `env:"RESTORE" envDefault:"true"`
+	Restore       bool          `env:"RESTORE"`
 }
 
 func (c *Config) loadConfiguration() error {
 	fAdd := flag.String("a", dAddress, "host:port")
-	fRestore := flag.Bool("r", dRestore, "restore previous metrics")
+
 	fStInetrval := flag.Duration("i", dStoreInterval, "an interval between metrics storing")
 	fStFile := flag.String("f", dStFile, "storage file address")
+
+	flag.BoolVar(&c.Restore, "r", true, "restore data from a file in -f")
 	flag.Parse()
 
 	if err := env.Parse(c); err != nil {
@@ -41,9 +43,6 @@ func (c *Config) loadConfiguration() error {
 	}
 	if c.StoreInterval == dStoreInterval && fStInetrval != nil && *fStInetrval != dStoreInterval {
 		c.StoreInterval = *fStInetrval
-	}
-	if c.Restore && fRestore != nil && !*fRestore {
-		c.Restore = *fRestore
 	}
 
 	log.Println(c)
